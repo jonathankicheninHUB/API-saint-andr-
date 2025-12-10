5a5fe8c19e79fc4e14c6824e2fb8172db16bfe45
jonathan kichenin <jonathan.kichenin@gmail.com>
2025-12-11 00:58:47 +0100

    Update settings.py

--- scraper_backend/scraper/settings.py
+++ scraper_backend/scraper/settings.py
@@ -1,9 +1,19 @@
-BOT_NAME = 'scraper_backend'
-SPIDER_MODULES = ['scraper_backend.scraper.spiders']
-NEWSPIDER_MODULE = 'scraper_backend.scraper.spiders'
+# Nom du bot
+BOT_NAME = 'scraper'
+
+# Où sont les araignées (Spiders) ?
+# On est déjà dans le dossier, donc on pointe directement vers 'scraper.spiders'
+SPIDER_MODULES = ['scraper.spiders']
+NEWSPIDER_MODULE = 'scraper.spiders'
+
+# On ignore le robots.txt pour être sûr d'avoir les données (à utiliser avec responsabilité)
 ROBOTSTXT_OBEY = False
 
-# C'est cette ligne qui active votre script pipelines.py
+# Activation du Pipeline vers Google Drive
 ITEM_PIPELINES = {
-   'scraper_backend.scraper.pipelines.MasterDataPipeline': 300,
+   'scraper.pipelines.MasterDataPipeline': 300,
 }
+
+# Réglages pour éviter d'être banni (Politesse)
+DOWNLOAD_DELAY = 1
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
