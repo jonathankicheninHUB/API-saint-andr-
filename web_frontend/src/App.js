86c5b88e9fce37fef5edd20059a9c54ca43e07b4
jonathan kichenin <jonathan.kichenin@gmail.com>
2025-12-10 21:08:41 +0100

    Create App.js

---
+++ web_frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dashboard from './views/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
